cae7f9a80c9fd61dee2e28c1cb8cd9472a58f502
Goutham Indukuri <indukuri.goutham@gmail.com>
2026-01-03 10:20:00 +0000

    refactor: extract shared constants to dedicated module

---
+++ apps/extension/src/constants.ts
@@ -0,0 +1,3 @@
+export const OVERRIDES_KEY = 'retractcheck:overrides';
+export const GITHUB_REPO_URL = 'https://github.com/gouthamindukuri/RetractCheck-Extension';
+export const SITE_REQUEST_URL = `${GITHUB_REPO_URL}/issues/new?title=Site%20support%20request`;
